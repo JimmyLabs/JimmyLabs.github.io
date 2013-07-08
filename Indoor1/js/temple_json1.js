2ade13bee9cdf0e04810b7bef1871066b7e39a13
Jimmy Yu <yujingming@gmail.com>
2013-07-08 17:38:51 +0800

    dhhh

---
+++ Indoor1/js/temple_json1.js
@@ -0,0 +1,15 @@
+({
+    "geoItems":[
+        {"id":"mark", "transparent":false, "opacity":1, "edit":false, "path":"./model/yuanzhui.js"},
+		{"id":"bangongyongpin", "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongyongpin.js"},
+        {"id":"bangongzhuo", "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongzhuo.js"},
+        {"id":"guizi", "X":2.719, "Y":-5.075, "Z":0.835, "transparent":false, "opacity":1, "edit":true, "path":"./model/guizi.js"},
+        {"id":"shafa", "X":-2.299, "Y":-4.996, "Z":0.457, "transparent":false, "opacity":1, "edit":true, "path":"./model/shafa.js"},
+        {"id":"kongtiao", "X":-10.382, "Y":1.791, "Z":0.848, "transparent":false, "opacity":1, "edit":true, "path":"./model/kongtiao.js"},
+        {"id":"tianhuaban", "transparent":false, "opacity":1, "edit":false, "path":"./model/tianhuaban.js"},
+        {"id":"qiangti", "transparent":false, "opacity":1, "edit":false, "path":"./model/qiangti.js"},
+        {"id":"chuangkuang", "transparent":false, "opacity":1, "edit":false, "path":"./model/chuangkuang.js"},
+        {"id":"dimian", "transparent":false, "opacity":1, "edit":false, "path":"./model/dimian.js"},
+        {"id":"men", "transparent":true, "opacity":0.5, "edit":false, "path":"./model/men.js"}
+    ]
+})
---
+++ Indoor1/model/banggongyongpin2.js
@@ -0,0 +1,29 @@
+{
+
+"metadata":
+{
+"sourceFile": "bangongshi.max",
+"generatedBy": "3ds max ThreeJSExporter",
+"formatVersion": 3,
+"vertices": 0,
+"normals": 0,
+"colors": 0,
+"uvs": 0,
+"triangles": 0,
+"materials": 0
+},
+
+"materials": [
+],
+
+"vertices": [],
+
+"normals": [],
+
+"colors": [],
+
+"uvs": [[]],
+
+"faces": []
+
+}
