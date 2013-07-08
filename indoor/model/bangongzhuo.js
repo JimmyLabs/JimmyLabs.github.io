e8be20f0e79e9cfc87e8c0750e554cd312ab8d17
Jimmy Yu <yujingming@gmail.com>
2013-07-08 15:01:37 +0800

    hgh

--- indoor/model/bangongzhuo.js
+++ indoor/model/bangongzhuo.js
@@ -22,7 +22,7 @@
 "colorSpecular"  : [0.9000, 0.9000, 0.9000],
 "transparency"  : 1.0,
 "specularCoef"  : 30.0,
-"mapDiffuse"    : "secai1.JPG",
+"mapDiffuse"    : "./secai1.JPG",
 "vertexColors" : false
 }
 
