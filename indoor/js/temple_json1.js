661864b98b2f8857bca7392b00a05e5a40947ab6
Jimmy Yu <yujingming@gmail.com>
2013-07-11 13:49:49 +0800

    修改模型

--- indoor/js/temple_json1.js
+++ indoor/js/temple_json1.js
@@ -1,15 +1,18 @@
 ({
     "geoItems":[
         {"id":"mark", "transparent":false, "opacity":1, "edit":false, "path":"./model/yuanzhui.js"},
-        {"id":"bangongyongpin", "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongyongpin.js"},
-        {"id":"bangongzhuo", "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongzhuo.js"},
-        {"id":"guizi", "X":2.719, "Y":-5.075, "Z":0.835, "transparent":false, "opacity":1, "edit":true, "path":"./model/guizi.js"},
+        {"id":"bangongyongpin", "X":-6.868, "Y":0.04698, "Z":1, "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongyongpin.js"},
+        {"id":"bangongzhuo",  "X":-6.868, "Y":0.04698, "Z":0, "transparent":false, "opacity":1, "edit":false, "path":"./model/bangongzhuo.js"},
         {"id":"shafa", "X":-2.299, "Y":-4.996, "Z":0.457, "transparent":false, "opacity":1, "edit":true, "path":"./model/shafa.js"},
         {"id":"kongtiao", "X":-10.382, "Y":1.791, "Z":0.848, "transparent":false, "opacity":1, "edit":true, "path":"./model/kongtiao.js"},
+        {"id":"guizi", "X":-1.319, "Y":-5.765, "Z":0.835, "transparent":false, "opacity":1, "edit":true, "path":"./model/guizi.js"},
+
         {"id":"tianhuaban", "transparent":false, "opacity":1, "edit":false, "path":"./model/tianhuaban.js"},
         {"id":"qiangti", "transparent":false, "opacity":1, "edit":false, "path":"./model/qiangti.js"},
         {"id":"chuangkuang", "transparent":false, "opacity":1, "edit":false, "path":"./model/chuangkuang.js"},
         {"id":"dimian", "transparent":false, "opacity":1, "edit":false, "path":"./model/dimian.js"},
-        {"id":"men", "transparent":true, "opacity":0.5, "edit":false, "path":"./model/men.js"}
+        {"id":"men", "transparent":true, "opacity":0.5, "edit":false, "path":"./model/men.js"},
+		{"id":"huiyizhuo", "transparent":false, "opacity":1, "edit":false, "path":"./model/huiyizhuo.js"},
+		{"id":"menbashou", "transparent":false, "opacity":1, "edit":false, "path":"./model/menbashou.js"}
     ]
 })
